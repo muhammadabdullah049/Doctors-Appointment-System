e34c9a943f828286f8afbe7d59336391856399b4
muhammadabdullah049 <muhammadabdullah2612049@gmail.com>
2024-11-06 02:37:05 +0500

    resolve error

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
@@ -8,18 +9,6 @@ const thumbsContainer = {
   flexWrap: "wrap",
   marginTop: 16,
 };
-
-// const thumb = {
-//   display: "inline-flex",
-//   borderRadius: 2,
-//   border: "1px solid #eaeaea",
-//   marginBottom: 8,
-//   marginRight: 8,
-//   width: 100,
-//   height: 100,
-//   padding: 4,
-//   boxSizing: "border-box",
-// };
 
 const thumbInner = {
   display: "flex",
@@ -51,12 +40,27 @@ export default function UploadImage() {
   });
 
   const thumbs = files.map((file) => (
-    <div style={thumb} key={file.name}>
+    <div
+      style={{
+        display: "inline-flex",
+        borderRadius: 2,
+        border: "1px solid #eaeaea",
+        marginBottom: 8,
+        marginRight: 8,
+        width: 100,
+        height: 100,
+        padding: 4,
+        boxSizing: "border-box",
+      }}
+      key={file.name}
+    >
       <div style={thumbInner}>
-        <img
+        <Image
           src={file.preview}
           style={img}
-          // Revoke data uri after image is loaded
+          alt="" // Provide alt text or an empty string if decorative
+          width={100} // Specify width and height explicitly for next/image
+          height={100}
           onLoad={() => {
             URL.revokeObjectURL(file.preview);
           }}
@@ -66,12 +70,14 @@ export default function UploadImage() {
   ));
 
   useEffect(() => {
-    // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+    // Revoke data URIs on component unmount to avoid memory leaks
     return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
   }, [files]);
+
   return (
     <section className="container">
       <div
+        {...getRootProps({ className: "dropzone" })}
         style={{
           display: "inline-flex",
           borderRadius: 2,
@@ -83,10 +89,9 @@ export default function UploadImage() {
           padding: 4,
           boxSizing: "border-box",
         }}
-        {...getRootProps({ className: "dropzone" })}
       >
         <input {...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
       </div>
       <aside style={thumbsContainer}>{thumbs}</aside>
     </section>
